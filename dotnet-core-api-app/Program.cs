d8aa810348926b2f58bc27bfe4b200cb7963ecfc
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2018-12-19 12:33:10 +0200

    Self-contained 'Hello world' console for Alpine Linux

---
+++ dotnet-core-api-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_api_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
