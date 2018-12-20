948a86389418616ab5b3c96df373e0bde9120919
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2018-12-20 12:36:35 +0200

    Kernel and Rootfs images download

--- scripts/fc-setup.sh
+++ scripts/fc-setup.sh
@@ -9,3 +9,7 @@ firecracker --version
 
 # ensure access to KVM (after each reboot)
 sudo setfacl -m u:${USER}:rw /dev/kvm
+
+# get sample rootfs and kernel
+curl -fsSL -o simple-rootfs.ext4 https://s3.amazonaws.com/spec.ccfc.min/img/hello/fsfiles/hello-rootfs.ext4
+curl -fsSL -o vmlinux.bin https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
